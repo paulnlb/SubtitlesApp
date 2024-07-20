d5c09a090d32c832ad4e6b35755730d433b4af47
Paul <doesntexist514@gmail.com>
2024-07-20 21:39:52 +0300

    feature(card-11): add mocker hub

---
+++ Server/SubtitlesServer/Hubs/WhisperMockHub.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SubtitlesApp.Core.Models;
+using SubtitlesApp.Shared.DTOs;
+using SubtitlesServer.Application.Interfaces;
+
+namespace SubtitlesServer.Hubs;
+
+public class WhisperMockHub : Hub
+{
+    public async Task TranscribeAudio(
+        IAsyncEnumerable<byte[]> dataChunks,
+        TrimmedAudioMetadataDTO audioMetadata)
+    {
+        Console.WriteLine("Connected");
+
+        await Clients.Caller.SendAsync("SetStatus", "Transcribing...");
+
+        await Task.Delay(1000);
+
+        for (int i = 0; i < 30; i+=2)
+        {
+            TimeSpan startTime;
+            TimeSpan endTime;
+
+            if (i % 10 == 0)
+            {
+                startTime = TimeSpan.FromSeconds(audioMetadata.StartTimeOffset.TotalSeconds + i + 1);
+                endTime = TimeSpan.FromSeconds(audioMetadata.StartTimeOffset.TotalSeconds + i + 3);
+            }
+            else
+            {
+                startTime = TimeSpan.FromSeconds(audioMetadata.StartTimeOffset.TotalSeconds + i);
+                endTime = TimeSpan.FromSeconds(audioMetadata.StartTimeOffset.TotalSeconds + i + 2);
+            }
+
+            var text = $"Subtitle ({startTime.Minutes}m, {startTime.Seconds}s)";
+
+            var subtitle = new SubtitleDTO
+            {
+                TimeInterval = new TimeIntervalDTO() { StartTime = startTime, EndTime = endTime },
+                Text = text
+            };
+
+            Console.WriteLine($"{subtitle.TimeInterval.StartTime}: {subtitle.Text}");
+
+            await Clients.Caller.SendAsync("ShowSubtitle", subtitle);
+        }
+
+        await Clients.Caller.SendAsync("SetStatusAndEditTimeline", "Done.", audioMetadata);
+    }
+
+    public async Task CancelTranscription([FromKeyedServices("cancellationManager")] ICancellationManager cancellationManager)
+    {
+        await Clients.Caller.SendAsync("SetStatus", "Transcritption cancelled.");
+    }
+}
