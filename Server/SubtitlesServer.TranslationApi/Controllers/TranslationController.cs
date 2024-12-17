3e11638eeaaf2f157e5fd0fbce447e3f431cede5
Paul <doesntexist514@gmail.com>
2024-12-17 22:51:36 +0200

    feature(card-33); add translation server logic

---
+++ Server/SubtitlesServer.TranslationApi/Controllers/TranslationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SubtitlesApp.Core.DTOs;
+using SubtitlesApp.Core.Services;
+using SubtitlesServer.Application.Interfaces;
+
+namespace SubtitlesServer.TranslationApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TranslationController(
+    ITranslationService translationService,
+    LanguageService languageService) : ControllerBase
+{
+    [HttpPost("translate")]
+    public async Task<IActionResult> Translate([FromBody] TranslationRequestDto request)
+    {
+        var language = languageService.GetLanguageByCode(request.TargetLanguageCode);
+
+        if (language == null)
+        {
+            return BadRequest("Invalid target language");
+        }
+
+        if (request.SourceSubtitles == null || !request.SourceSubtitles.Any())
+        {
+            return BadRequest("Source subtitles are required");
+        }
+
+        return Ok(await translationService.TranslateAsync(request));
+    }
+}
---
+++ Server/SubtitlesServer.TranslationApi/Program.cs
@@ -0,0 +1,33 @@
+﻿using SubtitlesServer.TranslationApi.Extensions;
+using SubtitlesServer.TranslationApi.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAppServices();
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+builder.Services.AddJwtAuthentication(builder.Configuration);
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+app.UseExceptionHandler();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
