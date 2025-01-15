5b85e9a1447fcb742d71776983e4bbf991920c50
Paul <doesntexist514@gmail.com>
2025-01-15 12:30:40 +0200

    chore: various remove unused whisper endpoint setting from settings menu

--- Client/SubtitlesApp.MAUI/ViewModels/SettingsViewModel.cs
+++ Client/SubtitlesApp.MAUI/ViewModels/SettingsViewModel.cs
@@ -1,48 +1,26 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using SubtitlesApp.Interfaces;
-using SubtitlesApp.Settings;
 
 namespace SubtitlesApp.ViewModels;
 
-public class SettingsViewModel : ObservableObject
+public partial class SettingsViewModel(ISettingsService settingsService) : ObservableObject
 {
-    private readonly ISettingsService _settingsService;
-    private readonly bool _isDevelopment;
-
-    public SettingsViewModel(ISettingsService settingsService)
-    {
-        _settingsService = settingsService;
-        _isDevelopment = settingsService is SettingsServiceDevelopment;
-    }
-
     public string BackendBaseUrl
     {
-        get => _settingsService.BackendBaseUrl;
+        get => settingsService.BackendBaseUrl;
         set
         {
-            _settingsService.BackendBaseUrl = value;
+            settingsService.BackendBaseUrl = value;
             OnPropertyChanged();
         }
     }
 
     public int TranscribeBufferLength
     {
-        get => _settingsService.TranscribeBufferLength;
+        get => settingsService.TranscribeBufferLength;
         set
         {
-            _settingsService.TranscribeBufferLength = value;
-            OnPropertyChanged();
-        }
-    }
-
-    public bool IsDevelopment => _isDevelopment;
-
-    public string WhisperAddress
-    {
-        get => _settingsService.TranscriptionPath;
-        set
-        {
-            _settingsService.TranscriptionPath = value;
+            settingsService.TranscribeBufferLength = value;
             OnPropertyChanged();
         }
     }
