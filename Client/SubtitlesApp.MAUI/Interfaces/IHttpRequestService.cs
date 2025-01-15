ef789cbad671bab82982df1706e0826241568ab6
Paul <doesntexist514@gmail.com>
2025-01-15 16:39:40 +0200

    chore: refactor http request service

--- Client/SubtitlesApp.MAUI/Interfaces/IHttpRequestService.cs
+++ Client/SubtitlesApp.MAUI/Interfaces/IHttpRequestService.cs
@@ -4,14 +4,34 @@ namespace SubtitlesApp.Interfaces;
 
 public interface IHttpRequestService
 {
+    /// <summary>
+    /// Sends an HTTP request and retrieves the result.
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>A <see cref="Result"/> indicating the outcome of the operation.</returns>
     Task<Result> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken = default);
 
+    /// <summary>
+    /// Sends an HTTP request and retrieves the result, with the response deserialized into a specified type.
+    /// </summary>
+    /// <typeparam name="TResultItem">The type into which the response will be deserialized. Must be a reference type with a parameterless constructor.</typeparam>
+    /// <param name="request"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>A <see cref="Result{TResultItem}"/> containing the deserialized response or an error.</returns>
     Task<Result<TResultItem>> SendAsync<TResultItem>(
         HttpRequestMessage request,
         CancellationToken cancellationToken = default
     )
         where TResultItem : class, new();
 
+    /// <summary>
+    /// Sends an HTTP request and retrieves the result as a stream of items of a specified type.
+    /// </summary>
+    /// <typeparam name="TResultItem">The type of items in the response stream.</typeparam>
+    /// <param name="request"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>An <see cref="AsyncEnumerableResult{TResultItem}"/> containing the streamed response items or an error.</returns>
     Task<AsyncEnumerableResult<TResultItem>> StreamAsync<TResultItem>(
         HttpRequestMessage request,
         CancellationToken cancellationToken = default
--- Client/SubtitlesApp.MAUI/Services/HttpRequestService.cs
+++ Client/SubtitlesApp.MAUI/Services/HttpRequestService.cs
@@ -20,149 +20,48 @@ public class HttpRequestService : IHttpRequestService
         _authService = authService;
     }
 
-    public async Task<Result> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken = default)
+    public Task<Result> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken = default)
     {
-        try
-        {
-            var token = await _authService.GetAccessTokenAsync();
-
-            if (!string.IsNullOrEmpty(token))
-            {
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            }
-
-            var response = await _httpClient.SendAsync(request, cancellationToken: cancellationToken);
-
-            if (response.IsSuccessStatusCode)
-            {
-                return Result.Success();
-            }
-
-            var error = await ConvertToErrorAsync(response, cancellationToken);
-
-            // If token expired, try to refresh it and then call SendAsync again
-            if (error.Code == ErrorCode.TokenExpired)
-            {
-                var refreshResult = await _authService.RefreshAccessTokenAsync();
-
-                if (refreshResult.IsSuccess)
-                {
-                    return await SendAsync(await request.CloneAsync(), cancellationToken);
-                }
-                else
-                {
-                    error = refreshResult.Error;
-                }
-            }
-
-            return Result.Failure(error);
-        }
-        catch (Exception ex)
-        {
-            var error = ConvertExceptionToError(ex);
-            return Result.Failure(error);
-        }
+        return SendAsyncInternal(request, successResponse => Task.FromResult(Result.Success()), cancellationToken);
     }
 
-    public async Task<Result<T>> SendAsync<T>(HttpRequestMessage request, CancellationToken cancellationToken = default)
+    public Task<Result<T>> SendAsync<T>(HttpRequestMessage request, CancellationToken cancellationToken = default)
         where T : class, new()
     {
-        try
-        {
-            var token = await _authService.GetAccessTokenAsync();
-
-            if (!string.IsNullOrEmpty(token))
+        return SendAsyncInternal(
+            request,
+            async successResponse =>
             {
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            }
-
-            var response = await _httpClient.SendAsync(request, cancellationToken: cancellationToken);
-
-            if (response.IsSuccessStatusCode)
-            {
-                var responseContent = await response.Content.ReadFromJsonAsync<T>(cancellationToken) ?? new();
-
+                var responseContent = await successResponse.Content.ReadFromJsonAsync<T>(cancellationToken) ?? new();
                 return Result<T>.Success(responseContent);
-            }
-
-            var error = await ConvertToErrorAsync(response, cancellationToken);
-
-            // If token expired, try to refresh it and then call SendAsync again
-            if (error.Code == ErrorCode.TokenExpired)
-            {
-                var refreshResult = await _authService.RefreshAccessTokenAsync();
-
-                if (refreshResult.IsSuccess)
-                {
-                    return await SendAsync<T>(await request.CloneAsync(), cancellationToken);
-                }
-                else
-                {
-                    error = refreshResult.Error;
-                }
-            }
-
-            return Result<T>.Failure(error);
-        }
-        catch (Exception ex)
-        {
-            var error = ConvertExceptionToError(ex);
-            return Result<T>.Failure(error);
-        }
+            },
+            cancellationToken
+        );
     }
 
-    public async Task<AsyncEnumerableResult<T>> StreamAsync<T>(
+    public Task<AsyncEnumerableResult<T>> StreamAsync<T>(
         HttpRequestMessage request,
         CancellationToken cancellationToken = default
     )
     {
-        try
-        {
-            var token = await _authService.GetAccessTokenAsync();
-
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
-
-            var response = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
-
-            if (response.IsSuccessStatusCode)
+        return SendAsyncInternal(
+            request,
+            successResponse =>
             {
                 var deserializeOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
-                var results = JsonSerializer.DeserializeAsyncEnumerable<T>(
-                    await response.Content.ReadAsStreamAsync(cancellationToken),
+                var deserializedObjects = successResponse.Content.ReadFromJsonAsAsyncEnumerable<T>(
                     deserializeOptions,
                     cancellationToken: cancellationToken
                 );
+                var result = AsyncEnumerableResult<T>.Success(RemoveNullsFrom(deserializedObjects));
 
-                return AsyncEnumerableResult<T>.Success(RemoveNulls(results));
-            }
+                return Task.FromResult(result);
+            },
+            cancellationToken
+        );
 
-            var error = await ConvertToErrorAsync(response, cancellationToken);
-
-            // If token expired, try to refresh it and then call SendAsync again
-            if (error.Code == ErrorCode.TokenExpired)
-            {
-                var refreshResult = await _authService.RefreshAccessTokenAsync();
-
-                if (refreshResult.IsSuccess)
-                {
-                    return await StreamAsync<T>(await request.CloneAsync(), cancellationToken);
-                }
-                else
-                {
-                    error = refreshResult.Error;
-                }
-            }
-
-            return AsyncEnumerableResult<T>.Failure(error);
-        }
-        catch (Exception ex)
-        {
-            var error = ConvertExceptionToError(ex);
-            return AsyncEnumerableResult<T>.Failure(error);
-        }
-
-        static async IAsyncEnumerable<T> RemoveNulls(IAsyncEnumerable<T?> source)
+        static async IAsyncEnumerable<T> RemoveNullsFrom(IAsyncEnumerable<T?> source)
         {
             await foreach (var item in source)
             {
@@ -176,69 +75,93 @@ public class HttpRequestService : IHttpRequestService
         }
     }
 
-    private static async Task<Error> ConvertToErrorAsync(
-        HttpResponseMessage response,
+    private async Task<TResult> SendAsyncInternal<TResult>(
+        HttpRequestMessage request,
+        Func<HttpResponseMessage, Task<TResult>> successResponseHandler,
         CancellationToken cancellationToken = default
     )
+        where TResult : Result
     {
-        Error? error = null;
-
         try
         {
-            error = await response.Content.ReadFromJsonAsync<Error>(cancellationToken);
+            var token = await _authService.GetAccessTokenAsync();
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            var response = await _httpClient.SendAsync(request, cancellationToken: cancellationToken);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await successResponseHandler(response);
+            }
+
+            var error = await ConvertToErrorAsync(response, cancellationToken);
+
+            // If token expired, try to refresh it and then call this method recursively
+            if (error.Code == ErrorCode.TokenExpired)
+            {
+                var refreshResult = await _authService.RefreshAccessTokenAsync();
+
+                if (refreshResult.IsSuccess)
+                {
+                    return await SendAsyncInternal(await request.CloneAsync(), successResponseHandler, cancellationToken);
+                }
+                else
+                {
+                    error = refreshResult.Error;
+                }
+            }
+
+            return Result.Failure(error) as TResult ?? throw new InvalidOperationException();
+        }
+        catch (Exception ex)
+        {
+            var error = ConvertExceptionToError(ex);
+            return Result.Failure(error) as TResult ?? throw new InvalidOperationException();
+        }
+    }
+
+    private static async Task<Error> ConvertToErrorAsync(HttpResponseMessage response, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var error = await response.Content.ReadFromJsonAsync<Error>(cancellationToken);
+            return error ?? ConvertStatusCodeToError(response.StatusCode);
         }
         catch
         {
-            // Do nothing
+            return ConvertStatusCodeToError(response.StatusCode);
         }
+    }
 
-        if (response.IsSuccessStatusCode)
+    private static Error ConvertStatusCodeToError(HttpStatusCode statusCode)
+    {
+        return statusCode switch
         {
-            throw new InvalidOperationException("Cannot convert successful response to error");
-        }
-        else if (response.StatusCode == HttpStatusCode.BadRequest)
-        {
-            error ??= new Error(ErrorCode.BadRequest, "Something is wrong with your request to the server");
-        }
-        else if (response.StatusCode == HttpStatusCode.Unauthorized)
-        {
-            error ??= new Error(ErrorCode.Unauthorized, "Authentication error. Check your credentials and try again");
-        }
-        else if (response.StatusCode == HttpStatusCode.Forbidden)
-        {
-            error ??= new Error(ErrorCode.Forbidden, "You cannot access this resource");
-        }
-        else
-        {
-            error = new Error(
-                ErrorCode.InternalServerError,
-                "Something has broken on the server side. Please try again later"
-            );
-        }
-
-        return error;
+            HttpStatusCode.BadRequest => new(ErrorCode.BadRequest, "Something is wrong with your request to the server"),
+            HttpStatusCode.Unauthorized => new(
+                ErrorCode.Unauthorized,
+                "Authentication error. Check your credentials and try again"
+            ),
+            HttpStatusCode.Forbidden => new(ErrorCode.Forbidden, "You cannot access this resource"),
+            _ => new(ErrorCode.InternalServerError, "Something has broken on the server side. Please try again later"),
+        };
     }
 
     private static Error ConvertExceptionToError(Exception ex)
     {
-        if (ex is OperationCanceledException)
+        return ex switch
         {
-            return new Error(ErrorCode.OperationCanceled, "Operation cancelled");
-        }
-        else if (ex is HttpRequestException)
-        {
-            return new Error(
+            OperationCanceledException => new(ErrorCode.OperationCanceled, "Operation cancelled"),
+            HttpRequestException => new(
                 ErrorCode.ConnectionError,
                 "Error while connecting to the server. Check your connection and try again"
-            );
-        }
-        else if (ex is WebException)
-        {
-            return new Error(ErrorCode.ConnectionError, $"Error while connecting to the server: {ex.Message}");
-        }
-        else
-        {
-            return new Error(ErrorCode.InternalClientError, $"An unknown error has occurred. {ex.Message}");
-        }
+            ),
+            WebException => new(ErrorCode.ConnectionError, $"Error while connecting to the server: {ex.Message}"),
+            _ => new(ErrorCode.InternalClientError, $"An unknown error has occurred. {ex.Message}"),
+        };
     }
 }
