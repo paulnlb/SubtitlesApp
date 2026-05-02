6ec0b44745ed6ee9cbac4f271588f65dab356b3a
Paul <pavlo.ohloblin@gmail.com>
2026-05-02 17:20:12 +0300

    feat: add regex validation for TimeSpan conversion in TimeSpanConverter

--- Client/SubtitlesApp.MAUI/Converters/TimeSpanConverter.cs
+++ Client/SubtitlesApp.MAUI/Converters/TimeSpanConverter.cs
@@ -1,9 +1,13 @@
 using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace SubtitlesApp.Converters;
 
-public class TimeSpanConverter : IValueConverter
+public partial class TimeSpanConverter : IValueConverter
 {
+    [GeneratedRegex(@"^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$")]
+    private static partial Regex TimeRegex();
+
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         return value is TimeSpan timeSpan ? timeSpan.ToString(@"hh\:mm\:ss") : string.Empty;
@@ -11,7 +15,12 @@ public class TimeSpanConverter : IValueConverter
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        if (TimeSpan.TryParse(value as string, out TimeSpan timeSpan))
+        if (value is not string time)
+        {
+            return Binding.DoNothing;
+        }
+
+        if (TimeRegex().IsMatch(time) && TimeSpan.TryParse(time, out TimeSpan timeSpan))
             return timeSpan;
 
         return Binding.DoNothing;
