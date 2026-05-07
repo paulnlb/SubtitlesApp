6727e677b86149622c0b89ceaf364b3f0dfac464
Paul <pavlo.ohloblin@gmail.com>
2026-05-07 21:55:27 +0300

    chore: Remove TranslationApi, BFF, IdentityApi

--- Server/SubtitlesServer.IdentityApi/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using SubtitlesServer.IdentityApi;
-
-var builder = WebApplication.CreateBuilder(args);
-
-var app = await builder
-    .ConfigureServices()
-    .ConfigurePipeline();
-
-app.Run();
