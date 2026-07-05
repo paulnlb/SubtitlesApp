d582e5ff00ec4f238acedbf8972a7c2ccc853e3a
Paul <pavlo.ohloblin@gmail.com>
2026-07-05 19:44:26 +0300

    chore: adjust popup sizes

--- Client/SubtitlesApp.MAUI/Helpers/ViewSizeHelper.cs
+++ Client/SubtitlesApp.MAUI/Helpers/ViewSizeHelper.cs
@@ -7,7 +7,7 @@ public static class ViewSizeHelper
     public static void SetPopupSize<T>(Popup<T> popup)
     {
         popup.MinimumHeightRequest = popup.MinimumWidthRequest = 100;
-        popup.MaximumHeightRequest = Math.Min(500, Shell.Current.CurrentPage.Height - 100);
-        popup.MaximumWidthRequest = Math.Min(400, Shell.Current.CurrentPage.Width - 50);
+        popup.MaximumHeightRequest = Math.Min(500, Shell.Current.CurrentPage.Height - 50);
+        popup.MaximumWidthRequest = Math.Min(400, Shell.Current.CurrentPage.Width - 100);
     }
 }
