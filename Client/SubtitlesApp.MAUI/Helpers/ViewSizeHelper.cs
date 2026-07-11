d87788d5d132d9f187f92c89f72a98eb51b8e069
Paul <pavlo.ohloblin@gmail.com>
2026-07-11 21:38:32 +0300

    fix: wrong popup sizes

--- Client/SubtitlesApp.MAUI/Helpers/ViewSizeHelper.cs
+++ Client/SubtitlesApp.MAUI/Helpers/ViewSizeHelper.cs
@@ -7,14 +7,14 @@ public static class ViewSizeHelper
     public static void SetPopupSize<T>(Popup<T> popup)
     {
         popup.MinimumHeightRequest = popup.MinimumWidthRequest = 100;
-        popup.MaximumHeightRequest = Math.Min(500, Shell.Current.CurrentPage.Height - 50);
-        popup.MaximumWidthRequest = Math.Min(400, Shell.Current.CurrentPage.Width - 100);
+        popup.MaximumHeightRequest = Math.Min(500, Shell.Current.Window.Height - 50);
+        popup.MaximumWidthRequest = Math.Min(350, Shell.Current.Window.Width - 50);
     }
 
     public static void SetWidePopupSize<T>(Popup<T> popup)
     {
         popup.MinimumHeightRequest = popup.MinimumWidthRequest = 100;
-        popup.MaximumHeightRequest = Math.Min(500, Shell.Current.CurrentPage.Height - 50);
-        popup.MaximumWidthRequest = Math.Min(700, Shell.Current.CurrentPage.Width - 100);
+        popup.MaximumHeightRequest = Math.Min(500, Shell.Current.Window.Height - 50);
+        popup.MaximumWidthRequest = Math.Min(700, Shell.Current.Window.Width - 50);
     }
 }
