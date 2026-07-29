4c77a4e4304b05761825e32dc323b152dd167dbb
Paul <pavlo.ohloblin@gmail.com>
2026-07-29 19:35:54 +0300

    chore: update log page by adding extra buttons; improve ux by adding visibility conditions for the buttons

---
+++ Client/SubtitlesApp.MAUI/Converters/AllTrueConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace SubtitlesApp.Converters;
+
+public class AllTrueConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values == null || values.Length == 0)
+        {
+            return false;
+        }
+
+        foreach (var value in values)
+        {
+            if (value is not bool boolValue || !boolValue)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        return [];
+    }
+}
--- Client/SubtitlesApp.MAUI/ViewModels/LogsPageViewModel.cs
+++ Client/SubtitlesApp.MAUI/ViewModels/LogsPageViewModel.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections.ObjectModel;
-using System.Text;
 using CommunityToolkit.Maui.Core.Extensions;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using SubtitlesApp.Constants;
-using SubtitlesApp.Core.Result;
 using SubtitlesApp.Interfaces;
 
 namespace SubtitlesApp.ViewModels;
@@ -12,30 +10,32 @@ namespace SubtitlesApp.ViewModels;
 public partial class LogsPageViewModel : ObservableObject
 {
     [ObservableProperty]
-    private ObservableCollection<string> _logPaths;
+    private ObservableCollection<string> _logFileNames;
 
     [ObservableProperty]
     private string _logsText = string.Empty;
 
     [ObservableProperty]
-    private string _selectedPath = string.Empty;
+    private string _selected = string.Empty;
 
     private readonly IBuiltInDialogService _dialogService;
 
+    private readonly string _basePath;
+
     public LogsPageViewModel(IBuiltInDialogService dialogService)
     {
-        _logPaths = Directory
-            .GetFiles(Path.Combine(FileSystem.Current.AppDataDirectory, FileConstants.LogsDir))
-            .ToObservableCollection();
+        _basePath = Path.Combine(FileSystem.Current.AppDataDirectory, FileConstants.LogsDir);
         _dialogService = dialogService;
+
+        LogFileNames = Directory.GetFiles(_basePath).Select(x => Path.GetFileName(x)).ToObservableCollection();
     }
 
     [RelayCommand]
     public async Task ClearLog()
     {
         var shouldDelete = await _dialogService.DisplayAlert(
-            "Clear Selected Log",
-            "You are about to clear the selected log file. Are you sure?",
+            "Clear Selected Logs File",
+            $"You are about to clear {Selected} Are you sure?",
             "Yes",
             "Cancel"
         );
@@ -45,44 +45,46 @@ public partial class LogsPageViewModel : ObservableObject
             return;
         }
 
-        await File.WriteAllTextAsync(SelectedPath, string.Empty);
+        var fullPath = Path.Combine(_basePath, Selected);
+        await File.WriteAllTextAsync(fullPath, string.Empty);
         LogsText = string.Empty;
     }
 
-    async partial void OnSelectedPathChanged(string value)
+    [RelayCommand]
+    public async Task LoadLog()
     {
-        try
+        if (string.IsNullOrEmpty(Selected))
         {
-            if (string.IsNullOrEmpty(value))
-            {
-                LogsText = string.Empty;
-                return;
-            }
-
-            const int MaxLogSize = 500 * 1024;
-            using var fileStream = File.OpenRead(value);
-
-            if (fileStream.Length > MaxLogSize)
-            {
-                await _dialogService.DisplayAlert(
-                    "Log file is too big",
-                    $"Selected log file is too big, only the last {MaxLogSize / 1024}KB will be loaded",
-                    "Ok"
-                );
-            }
-
-            var offset = Math.Max(0, fileStream.Length - MaxLogSize);
-            fileStream.Seek(offset, SeekOrigin.Begin);
-
-            using var streamReader = new StreamReader(fileStream);
-
-            LogsText = await streamReader.ReadToEndAsync();
+            LogsText = string.Empty;
+            return;
         }
-        catch (Exception ex)
+
+        var fullPath = Path.Combine(_basePath, Selected);
+        const int MaxLogSize = 500 * 1024;
+        using var fileStream = File.OpenRead(fullPath);
+
+        if (fileStream.Length > MaxLogSize)
         {
-            MainThread.BeginInvokeOnMainThread(async () =>
-                await _dialogService.DisplayError(new Error(ErrorCode.InternalClientError, ex.Message))
+            await _dialogService.DisplayAlert(
+                "Log file is too big",
+                $"Selected log file is too big, only the last {MaxLogSize / 1024}KB will be loaded",
+                "Ok"
             );
         }
+
+        var offset = Math.Max(0, fileStream.Length - MaxLogSize);
+        fileStream.Seek(offset, SeekOrigin.Begin);
+
+        using var streamReader = new StreamReader(fileStream);
+
+        LogsText = await streamReader.ReadToEndAsync();
+    }
+
+    [RelayCommand]
+    public async Task ExportLog() { }
+
+    partial void OnSelectedChanged(string value)
+    {
+        LogsText = string.Empty;
     }
 }
